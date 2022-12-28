7f1d406771c7b981e8e30811a4dbd9871084de06
Mashto-o <manyusechka@gmail.com>
2022-12-28 20:44:57 +0100

    changed actions

--- actions/actions.py
+++ actions/actions.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 import re
 from typing import Any, Text, Dict, List
-from pyparsing import nestedExpr
+
 from rasa_sdk import Tracker, FormValidationAction, Action
 from rasa_sdk.events import EventType, SlotSet
 from rasa_sdk.types import DomainDict
@@ -26,25 +26,6 @@ from rasa_sdk.executor import CollectingDispatcher
 
 load_dotenv()
 wakey= os.getenv("wakey") 
-
-class MainMenu(Action):
-    def name(self) -> Text:
-        return "action_get_weather"
-    def run(self, dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]) -> List[Dict[Text,Any]]:
-        city_name = tracker.get_slot("City")
-        base_url = "http://api.openweathermap.org/data/2.5/weather?"
-        complete_url = base_url + "appid=" + wakey+ "&q=" + city_name + "&units=metric" + "&lang=de"
-        response = requests.get(complete_url)
-        x = response.json()["main"]
-        desc = response.json()["weather"]
-        current_temperature = x["temp"]
-        weather_description = desc[0]["description"]
-        dispatcher.utter_button_message(f"In {city_name} sind es " + str(current_temperature) + "°C. \nAktueller Wetterstatus: " + str(weather_description))
-
-        return
-
 
 # NOTE(Michael): We could use this action to store the name in
 #                the TrackerStore (in memory database) or a persitent DB
@@ -78,7 +59,6 @@ class ActionUserName(Action):
 
         return []
 
-class API(Action):
 
 class Wetter(Action):
     def name(self) -> Text:
